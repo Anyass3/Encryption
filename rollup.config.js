dfaedb4015b85222c7de822b85dff1104f071d6f
Abdoulie Nyass <nyassabu@gmail.com>
2024-07-07 19:23:09 +0000

    updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,37 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
-import pkg from './package.json';
+import pkg from './package.json' with { type: 'json' };
 
 const name = pkg.name
-  .replace(/^(@\S+\/)?(\S+)/, '$3')
-  .replace(/^\w/, (m) => m.toUpperCase())
-  .replace(/-\w/g, (m) => m[1].toUpperCase());
+	.replace(/^(@\S+\/)?(\S+)/, '$3')
+	.replace(/^\w/, (m) => m.toUpperCase())
+	.replace(/-\w/g, (m) => m[1].toUpperCase());
 
 const production = !process.env.ROLLUP_WATCH;
 
-const config = (browser=true) => {
-  let output=browser?pkg.exports['.']:pkg.exports['.']['node']
-  return {
-    input: `src/${browser?'browser':'node'}.ts`,
-    output: [
-      { file: output.import, format: 'es' },
-      { file: output.require||output.default, format: 'umd', name },
-    ],
-    plugins: [
-      resolve({preferBuiltins: !browser, browser}),
-      commonjs(),
-      typescript({
-        sourceMap: !production,
-        inlineSources: !production,
-      }),
-      production && terser(),
-    ],
-    watch: {
-      clearScreen: false,
-    },
-  };
+const config = (browser = true) => {
+	let output = browser ? pkg.exports['.']['browser'] : pkg.exports['.']['default'];
+	return {
+		input: `src/${browser ? 'browser' : 'node'}.ts`,
+		output: [
+			{ file: output.import, format: 'es' },
+			{ file: output.require || output.default, format: 'umd', name }
+		],
+		plugins: [
+			resolve({ preferBuiltins: !browser, browser }),
+			commonjs(),
+			typescript({
+				sourceMap: !production,
+				inlineSources: !production
+			}),
+			production && terser()
+		],
+		watch: {
+			clearScreen: false
+		}
+	};
 };
 // console.log(config())
-export default [
-  config(true),
-  config(false)
-];
+export default [config(true), config(false)];
